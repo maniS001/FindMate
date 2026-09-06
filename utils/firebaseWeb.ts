3b49b84abe646726f93bfc80c2297a2bf2739a90
ManiS001 <mkingdom209@gmail.com>
2026-09-06 18:21:46 +0530

    Fix: Insert Web App ID for Web Firebase config

--- utils/firebaseWeb.ts
+++ utils/firebaseWeb.ts
@@ -2,11 +2,14 @@ import { initializeApp, getApps, getApp } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBfBd_6W9AndpyPJW0gPsBUvlyiG21UWjc",
+  apiKey: "AIzaSyArcVxFsd9hA6Z5eZr8sw35VVqAtdhYXCI",
   authDomain: "stproject-1dbc9.firebaseapp.com",
+  databaseURL: "https://stproject-1dbc9.firebaseio.com",
   projectId: "stproject-1dbc9",
   storageBucket: "stproject-1dbc9.firebasestorage.app",
   messagingSenderId: "1018607704367",
+  appId: "1:1018607704367:web:9fdd7d80becbb726b7a9da",
+  measurementId: "G-8MPP79EK23"
 };
 
 // Initialize Firebase only once
